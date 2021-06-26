4826d2cd8002123e309cc12eaf0fce7f432af4a7
nick_without_wick <nikolavujicic.etf@gmail.com>
2021-06-26 19:34:20 +0200

    Player and enemy added with the core functionalities.

---
+++ Honeybee Numbers/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Player : MonoBehaviour
+{
+    private float movement;
+    public Rigidbody2D rb;
+    private int number;
+    //public Transform movePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        number = Random.Range(2, 10);
+        GetComponentInChildren<TextMeshProUGUI>().text = number.ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (movement !=0)
+        {
+            if (gameObject.tag == "Player") {
+                transform.position = new Vector2(movement * 5, transform.position.y);
+            }
+            else
+            {
+                //rb.AddForce(new Vector2(1,0), ForceMode2D.Impulse);
+                rb.MovePosition(Vector2.zero);
+            }
+            
+        }
+         
+        
+    }
+}

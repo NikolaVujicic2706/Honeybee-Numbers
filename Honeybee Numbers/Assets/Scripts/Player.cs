9b3ca738f70d01f03318c55da0f73c7fde6ec3a5
nick_without_wick <nikolavujicic.etf@gmail.com>
2021-08-17 00:09:53 +0200

    Corutines used and lerp function implemented in SwipeManager script

--- Honeybee Numbers/Assets/Scripts/Player.cs
+++ Honeybee Numbers/Assets/Scripts/Player.cs
@@ -19,12 +19,16 @@ public class Player : MonoBehaviour
         }
         set { speed = value; }
     }
+    
+
+    //public float Speed { get; set; }
+
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         number = Random.Range(2, 10);
-        speed = 10f;
+        speed = 1000f;
         GetComponentInChildren<TextMeshProUGUI>().text = number.ToString();
     }
 
